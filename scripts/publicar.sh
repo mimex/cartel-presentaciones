8413eb43b47494b87cb07da09a9cd96468b4f27b
mimex <mimex@fipasoft.mx>
2014-10-01 12:28:26 -0500

    NEW: Actualizar PNG, SVG a curvas al generar los archivos de publicacion

--- scripts/publicar.sh
+++ scripts/publicar.sh
@@ -5,6 +5,23 @@ then
     fecha=$1
 fi
 
+# exportar a PNG
+inkscape carteles/$fecha-cartel.svg -w 640 -e carteles/${fecha}-cartel.png
+
+# convertir a curvas
+#!/bin/bash
+orig=carteles/${fecha}-cartel.svg
+curvas=carteles/${fecha}-cartel-curvas.svg
+cp $orig $curvas
+inkscape $curvas \
+     --verb=UnlockAllInAllLayers \
+     --verb=UnhideAllInAllLayers \
+     --verb=EditSelectAllInAllLayers \
+     --verb=ObjectToPath \
+     --verb=FileSave \
+     --verb=FileQuit
+
+# copiar archivos principales
 rm cartel-web.png cartel.svg
 cp carteles/$fecha-cartel-curvas.svg cartel.svg
 cp carteles/$fecha-cartel.png cartel-web.png
